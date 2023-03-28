43a4c3934471bf30fa8ebfd00eeb5eb76f887fd8
Yassmin-gamal <66153260+Yassmin-gamal@users.noreply.github.com>
2023-03-29 01:39:22 +0200

    Update blockListener.java

--- src/blockListener.java
+++ src/blockListener.java
@@ -1,6 +1,8 @@
 import org.antlr.v4.runtime.TokenStreamRewriter;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
+import java.io.IOException;
+
 public class blockListener extends JavaParserBaseListener{
     int i;
     TokenStreamRewriter rewriter;
@@ -8,16 +10,27 @@ public class blockListener extends JavaParserBaseListener{
         this.rewriter = rewriter;
         this.i=0;
     }
+
+    @Override public void enterMethodBody(JavaParser.MethodBodyContext ctx) {
+        rewriter.insertBefore(ctx.getStart(),"throws IOException ");
+        rewriter.insertAfter(ctx.getStart(),"\n"+"\t"+"File output = new File(\"output.txt\");"+"\n");
+        rewriter.insertAfter(ctx.getStart(),"\t"+"output.createNewFile();"+"\n");
+        rewriter.insertAfter(ctx.getStart(),"\t"+"FileWriter w = new FileWriter(\"output.txt\");"+"\n");
+        rewriter.insertBefore(ctx.getStop(),"w.close();"+"\n");
+
+    }
+
     @Override public void enterBlock(JavaParser.BlockContext ctx) {
         this.i++;
-        rewriter.insertAfter(ctx.getStart(),"//block number " + this.i);
+        rewriter.insertAfter(ctx.getStart()," //block"+ this.i +"\n");
+
+        rewriter.insertAfter(ctx.getStart(),"\t"+"w.write(\"block "+ this.i + " is visited\" +\"\\n\");"  +"\n");
+
+
 
     }
     @Override public void exitBlock(JavaParser.BlockContext ctx) {
 
     }
-    @Override public void visitTerminal(TerminalNode node) {
-        //System.out.println(node.getText());
 
-    }
 }
